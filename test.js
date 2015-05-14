b0d1ad350fca1738517019516897693956f85000
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-05-15 01:59:44 +0900

    write the test with ES.next code

--- test.js
+++ test.js
@@ -1,46 +1,44 @@
 'use strict';
 
-var EventEmitter = require('events').EventEmitter;
+const EventEmitter = require('events').EventEmitter;
 
-var modifyErrorEvent = require('./');
-var test = require('tape');
+const modifyErrorEvent = require('.');
+const test = require('tape');
 
-var emitter = new EventEmitter();
+let emitter = new EventEmitter();
 
-test('modifyErrorEvent()', function(t) {
+test('modifyErrorEvent()', t => {
   t.plan(7);
 
   t.equal(modifyErrorEvent.name, 'modifyErrorEvent', 'should have a function name.');
 
   t.strictEqual(
-    modifyErrorEvent(emitter, function(data) {
-      return data * 2;
-    }),
+    modifyErrorEvent(emitter, data => data * 2),
     emitter,
     'should return the modified EventEmitter.'
   );
 
   emitter
-  .on('error', function(data) {
+  .on('error', data => {
     t.strictEqual(this, emitter, 'should call the listener in the same context as original\'s.');
     t.strictEqual(data, 2, 'should modify the value of event.');
   })
   .emit('error', 1);
 
   t.throws(
-    modifyErrorEvent.bind(null, null, t.fail),
+    () => modifyErrorEvent(null, t.fail),
     /TypeError.* must be an instance of EventEmitter or its inheritance\./,
     'should throw a type error when the first argument is not an object.'
   );
 
   t.throws(
-    modifyErrorEvent.bind(null, {}, t.fail),
+    () => modifyErrorEvent({}, t.fail),
     /TypeError.* must be an instance of EventEmitter or its inheritance\./,
     'should throw a type error when the first argument is not an instance of EventEmitter.'
   );
 
   t.throws(
-    modifyErrorEvent.bind(null, emitter, 'foo'),
+    () => modifyErrorEvent(emitter, 'foo'),
     /TypeError.*foo is not a function. The third argument to modify-event must be a function\./,
     'should throw a type error when the third argument is not a function.'
   );
